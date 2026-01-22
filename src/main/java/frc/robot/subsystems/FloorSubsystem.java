84603641da3fbd25cfb5d9dbbed92632f21e7c05
Gelter317 <justin_pleffner@mynbps.org>
2026-01-21 19:27:36 -0500

    Added Floor Subsystem

---
+++ src/main/java/frc/robot/subsystems/FloorSubsystem.java
@@ -0,0 +1,60 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class FloorSubsystem extends SubsystemBase{
+
+    private SparkMax motor = new SparkMax(Constants.FloorConstants.canID, MotorType.kBrushless);
+
+    private static FloorSubsystem floorSingleton;
+    public static FloorSubsystem getInstance() {
+        if (floorSingleton==null) floorSingleton = new FloorSubsystem();
+        return floorSingleton;
+    }
+
+    public FloorSubsystem() {
+        SparkBaseConfig sharedConfig = new SparkMaxConfig().apply(Constants.kBrakeConfig).smartCurrentLimit(40, 40);
+        SparkBaseConfig motorConfig = new SparkMaxConfig().apply(sharedConfig);
+
+        for (int i = 0; i<=5; i++) {
+            motor.configure(motorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+        }
+    }
+
+
+
+    public void doFloorIntake() {
+        motor.set(Constants.FloorConstants.motorSpeed);
+    }
+
+    public void doFloorOuttake() {
+        motor.set(-Constants.FloorConstants.motorSpeed);
+    }
+
+    public void stopFloor() {
+        motor.set(-0.0);
+    }
+
+
+
+    public Command intakeCommand(){
+        return this.runOnce(()->doFloorIntake());
+    }
+
+    public Command outtakeCommand(){
+        return this.runOnce(()->doFloorOuttake());
+    }
+
+    public Command stopCommand(){
+        return this.runOnce(()->stopFloor());
+    }
+}
